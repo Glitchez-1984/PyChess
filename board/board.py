6fc579fd5d8fb0fe21a24f79a9a17ea4a26ee09e
David Safro <david.github24@gmail.com>
2023-04-30 22:26:23 -0400

    render pieces

--- board/board.py
+++ board/board.py
@@ -5,12 +5,22 @@ from board.square import Square
 from object import Object
 from controls import *
 from text import render_text
-
+from board.piece import Piece
 class Board(Object):
     def __init__(self, width, height, x, y):
         super().__init__(width, height, x, y)
         self.squares = np.zeros((ROW_LENGTH, ROW_LENGTH), dtype=object)
         self.board = pygame.Surface((BOARD_WIDTH, BOARD_HEIGHT))
+        self.pieces = np.array([
+    [Piece(3,"b"), Piece(1,"b"), Piece(2,"b"), Piece(4,"b"), Piece(5,"b"), Piece(2,"b"), Piece(1,"b"), Piece(3,"b")],
+    [Piece(0,"b"), Piece(0,"b"), Piece(0,"b"), Piece(0,"b"), Piece(0,"b"), Piece(0,"b"), Piece(0,"b"), Piece(0,"b")],
+    [None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None],
+    [Piece(0,"w"), Piece(0,"w"), Piece(0,"w"), Piece(0,"w"), Piece(0,"w"), Piece(0,"w"), Piece(0,"w"), Piece(0,"w")],
+    [Piece(3,"w"), Piece(1,"w"), Piece(2,"w"), Piece(4,"w"), Piece(5,"w"), Piece(2,"w"), Piece(1,"w"), Piece(3,"w")]
+])
 
     def create_board(self):
         for row in range(ROW_LENGTH):
@@ -24,6 +34,9 @@ class Board(Object):
             square_surface = self.board.subsurface(square.square_object)
             square_surface.fill(square.color)
             pos = np.where(self.squares == square)
+            piece = self.pieces[pos[0][0],pos[1][0]]
+            if piece is not None:
+                piece.draw(square_surface)
             if pos[1] == 0:
                 render_text(square_surface, str(ROW_LENGTH - pos[0])[1],"Segoe UI Semibold",18,True,255,5,5,LIGHT_SQUARE if square.color == DARK_SQUARE else DARK_SQUARE, False)
             if pos[0] ==  ROW_LENGTH-1:
--- board/piece.py
+++ board/piece.py
@@ -1,10 +1,11 @@
 import pygame
-
-
+import os
+from controls import *
 class Piece(object):
-    def __init__(self, width, height, x, y, img):
-        super().__init__(width, height, x, y)
-        self.img = img
-
+    def __init__(self, ptype, color):
+        self.type = ptype
+        self.color = color
+        img_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'imgs', f"{self.color}{self.type}.svg"))
+        self.image = pygame.transform.smoothscale(pygame.image.load(img_path), (SIDE, SIDE))
     def draw(self, win):
-        win.blit(self.img)
+        win.blit(self.image,(0,0))
